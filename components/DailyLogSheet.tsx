3ee7c34cebbe852e50a56fd810ca3eaea48dd3cb
papa bassirou ndiaye <pbnlgk@gmail.com>
2025-03-19 00:41:14 +0000

    fix error

--- components/DailyLogSheet.tsx
+++ components/DailyLogSheet.tsx
@@ -148,7 +148,6 @@ export default function DailyLogSheet({ logs, tripId }: DailyLogSheetProps) {
       const ctx = canvas.getContext('2d');
       if (ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        setDriverSignature(null);
       }
     }
   };
